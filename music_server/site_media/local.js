e0d112426cf914a763c04f228ac4e6fd5d86d011
Chris Lamb <lamby@debian.org>
2008-11-09 15:51:41 +0000

    Add AJAX swapping functionality.
    Signed-off-by: Chris Lamb <lamby@debian.org>

--- music_server/site_media/local.js
+++ music_server/site_media/local.js
@@ -1,3 +1,15 @@
+jQuery.fn.swap = function(b) {
+	b = jQuery(b)[0];
+	var a = this[0];
+
+	var t = a.parentNode.insertBefore(document.createTextNode(''), a);
+	b.parentNode.insertBefore(a, b);
+	t.parentNode.insertBefore(b, t);
+	t.parentNode.removeChild(t);
+
+	return this;
+};
+
 $(function() {
 	$('.delete-link').click(function() {
 		var item = $(this).parents('li');
@@ -18,4 +30,41 @@ $(function() {
 
 		return false;
 	});
+
+	$('.up-link').click(function() {
+		return move('up', this);
+	});
+
+	$('.down-link').click(function() {
+		return move('down', this);
+	});
+		
+	function move (direction, link) {
+		var item = $(link).parents('li');
+		var item_id = item.attr('id').split('-', 2)[1];
+
+		$('.ajax', item).show();
+
+		$.ajax({
+			url: '/a/item/' + direction + '/' + item_id,
+			success: function(data) {
+				if (data) {
+					var other_item = $('#item-' + data);
+					$(other_item).fadeOut(300, function() {
+						$(item).swap($(other_item));
+						$(item).fadeIn();
+						$(other_item).fadeIn();
+					});
+				} else {
+					location.reload();
+				}
+			},
+			error: function(request, textStatus, errorThrown) {
+				alert('Error');
+				$('.ajax', item).hide();
+			}
+		});
+
+		return false;
+	}
 });
